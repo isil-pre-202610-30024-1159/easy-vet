b96105d55a8a745b7eeef923f23676b20ece735c
jmayta1984 <jmayta@pucp.edu.pe>
2026-06-13 11:22:08 -0500

    feat: replace HomeScreen with MainScreen in MainActivity

--- app/src/main/java/pe/isil/easyvet/MainActivity.kt
+++ app/src/main/java/pe/isil/easyvet/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import pe.isil.easyvet.core.ui.theme.AppTheme
-import pe.isil.easyvet.features.home.presentation.HomeScreen
+import pe.isil.easyvet.features.main.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                HomeScreen()
+                MainScreen()
             }
         }
     }
